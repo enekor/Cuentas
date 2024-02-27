7a9e1bd67c3ba8e832a162a38b021f5d4f96336b
eneko <eneko12rebollo@gmail.com>
2024-02-27 10:29:00 +0100

    card

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/widgets/ItemView.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,16 +31,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -52,35 +43,16 @@ class _MyHomePageState extends State<MyHomePage> {
   void _changeText(String text) {
     setState(() {
       _text = text;
-    }); 
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("Bienvenido!"),
-            Text(_text),
-            Row(
-              children:[
-                ElevatedButton(
-                  child:Text("Ver cuentas"),
-                  onPressed: (){_changeText('Hola');},
-                ),
-              ]
-            )
-          ],
-        ),
-      ),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[ItemCard("a")])),
     );
   }
 }
---
+++ lib/widgets/ItemView.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+Widget ItemCard(_) => Card(
+      child: Column(
+        children: [
+          Text(("hola")),
+          ElevatedButton(onPressed: () {}, child: Text("boton"))
+        ],
+      ),
+    );
